cbeab1a9149df3096819408c9e150412719d6dc9
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-07-08 19:40:46 -0700

    init

---
+++ junior_runner_ble.py
@@ -0,0 +1,71 @@
+
+
+import logging
+from typing import List
+import asyncio
+
+try:
+    from bluetooth.ble import BLEConnection
+except:
+    from ROAR_Junior.bluetooth.ble import BLEConnection
+
+try:
+    from game.game import Game
+except:
+    from ROAR_Junior.game.game import Game
+
+
+async def main(tasks):
+    await asyncio.wait(tasks)
+
+
+def createBLETasks(ble_connection: BLEConnection) -> List:
+    """
+    Create Bluetooth related tasks
+    :param ble_connection: Bluetooth connection object
+    :return:
+        List of task that asyncio can start
+    """
+    return [loop.create_task(ble_connection.connectHelper()),
+            loop.create_task(ble_connection.startSendControl())]
+
+
+def createGameTask(game: Game) -> List:
+    """
+    Create game related Tasks
+    :param game: game object
+    :return:
+        List of task related to game
+    """
+    return [loop.create_task(game.run())]
+
+
+if __name__ == '__main__':
+
+    # define logging format
+    logging.basicConfig(format='%(levelname)s - %(asctime)s - %(name)s '
+                               '- %(message)s',
+                        datefmt="%H:%M:%S")
+
+    loop = asyncio.get_event_loop()
+    # you may get this by doing BLEConnection.scan() and copy the ID of the device you want to connect to.
+    deviceUUID = "095D2164-4A57-47B0-8857-BDA8537BBFA1"
+
+    # prelude: Initialize Game and BLE connection
+    connection: BLEConnection = BLEConnection(
+        loop=loop,
+        device_addr=deviceUUID,
+        debug=logging.DEBUG)
+    game = Game(ble_connection=connection, loop=loop, rate=0.025, debug_level=logging.DEBUG)
+    tasks = []
+    tasks.extend(createBLETasks(ble_connection=connection))
+    tasks.extend(createGameTask(game=game))
+
+    # start all tasks
+    try:
+        loop.run_until_complete(main(tasks))
+        loop.close()
+    except KeyboardInterrupt:
+        print("User Stopped Program")
+    finally:
+        connection.on_disconnect()
---
+++ keyboard_control.py
@@ -0,0 +1,55 @@
+import pygame
+from pygame.locals import *
+import logging
+from typing import Tuple
+
+
+class JuniorKeyboardControl(object):
+    def __init__(self, left_motor_increment=10, right_motor_increment=10, log_level=logging.DEBUG):
+        self.logger = logging.getLogger("JuniorKeyboard")
+        self.logger.setLevel(log_level)
+        self._left_motor_increment = left_motor_increment
+        self._right_motor_increment = right_motor_increment
+        self.left_motor = 0
+        self.right_motor = 0
+
+    def parse_events(self, clock: pygame.time.Clock) -> Tuple[bool, Tuple[int, int]]:
+        """
+        Outer layer will call this function on every step.
+
+        Pygame will query whether an event has happened.
+
+        If so, parse the event.
+
+        :param clock: pygame clock
+        :return:
+            [SHOULD_CONTINUE, [MOTOR_LEFT, MOTOR_RIGHT]
+        """
+        events = pygame.event.get()
+        key_pressed = pygame.key.get_pressed()
+        for event in events:
+            if event.type == pygame.QUIT or key_pressed[K_q] or key_pressed[K_ESCAPE]:
+                return False, (self.left_motor, self.right_motor)
+
+        self._parse_control_keys(key_pressed)
+        return True, (self.left_motor, self.right_motor)
+
+    def _parse_control_keys(self, keys):
+        """
+        Parse keyboard press
+
+        :param keys: List of keys being pressed
+        :return:
+            none
+        """
+        if keys[K_UP]:
+            self.right_motor = min(self.right_motor + self._right_motor_increment, 255)
+
+        elif keys[K_DOWN]:
+            self.right_motor = max(self.right_motor - self._right_motor_increment, 0)
+
+        if keys[K_w]:
+            self.left_motor = min(self.left_motor + self._left_motor_increment, 255)
+
+        elif keys[K_s]:
+            self.left_motor = max(self.left_motor - self._left_motor_increment, 0)
